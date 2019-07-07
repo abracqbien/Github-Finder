8337ba07a563b5ad0bb7bdd77dbbc7180e4f9e97
Alexandre <abracqbien.pro@laposte.net>
2019-07-07 15:00:11 +0200

    Home page & NotFound page

---
+++ app/src/components/pages/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notfound = () => {
+  return (
+    <div>
+      <h1>Not FOund</h1>
+      <p className='lead'>the page you are looking for does not exist...</p>
+    </div>
+  );
+};
+
+export default Notfound;
